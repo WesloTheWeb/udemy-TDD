2c797f68bf18bbfeac1252942264aa81f4a366bc
Wesley <wesleyadwebster@gmail.com>
2021-11-13 04:58:41 -0600

    Start of TDD, written first 5 test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders learn react link', () => {
- 
+test('renders a non-empty component without crashing', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.exists()).toBe(true);
+});
+
+// For TDD (Test Driven Development) we'd want to write our test cases first before code.
+
+test('renders increment button', () => {
+
+});
+
+test('shows the counter display', () => {
+
+});
+
+test('counter display starts at 0', () => {
+
+});
+
+test('clicking button increments counter display', () => {
+
 });
