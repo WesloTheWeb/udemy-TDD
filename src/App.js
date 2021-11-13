c22da7cd4737d2a9b485f801d9d5d4cbef594a55
Wesley <wesleyadwebster@gmail.com>
2021-11-13 17:43:30 -0600

    Cannot go below -1 when decrementing. Added error message if below zero

--- src/App.js
+++ src/App.js
@@ -7,14 +7,23 @@ function App() {
 
   const handleIncrementClick = () => setCount(count + 1);
 
-  const handleDecrementClick = () => setCount(count - 1);
+  const handleDecrementClick = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else 
+      setCount(-1);      
+  }
 
   return (
     <div data-test="component-app" className="App">
       <h1>Let's learn React testing</h1>
-      <h2 data-test="counter-display">The counter is currently&nbsp;
+      {count >= 0 ? <h2 data-test="counter-display">The counter is currently&nbsp;
         <span data-test="count">{count}</span>
-      </h2>
+      </h2> : null
+      }
+      {count < 0 ?
+        <div>ERROR! Cannot go below zero</div> : null
+      }
       <div className="button-container">
         <button
           data-test="increment-button"
